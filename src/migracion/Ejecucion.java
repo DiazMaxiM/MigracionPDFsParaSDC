c55b59dc9b4968d2b0444426cb79edff2c97464c
Maximiliano Diaz <diazmaxi@gmail.com>
2020-07-14 11:21:41 -0300

    commit inicial

---
+++ src/migracion/Ejecucion.java
@@ -0,0 +1,11 @@
+package migracion;
+
+public class Ejecucion {
+
+	public static void main(String[] args) {
+		String direccion = "./CarpetasImportantes/";
+		System.out.println(direccion);
+
+	}
+
+}
